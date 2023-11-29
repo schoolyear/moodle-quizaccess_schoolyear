a9659e833aa03dc79746f0d9e5bc60e4f87fd2e8
Balazs Gida <b.gida@schoolyear.com>
2023-11-29 16:10:36 +0100

    add privacy provider

---
+++ classes/privacy/provider.php
@@ -0,0 +1,8 @@
+<?php
+namespace quizaccess_schoolyear\privacy;
+
+class provider implements \core_privacy\local\metadata\null_provider {
+    public static function get_reason() : string {
+        return 'privacy:metadata';
+    }
+}
