103522e43672a6e949252a1bc5469f85aa7c3eff
Balazs Gida <b.gida@schoolyear.com>
2024-01-15 11:25:11 +0100

    bump plugin version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_schoolyear';
-$plugin->release = '1.0.1';
-$plugin->version = 2023121900;
+$plugin->release = '1.0.2';
+$plugin->version = 2024011500;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
