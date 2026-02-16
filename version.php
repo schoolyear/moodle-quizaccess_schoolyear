befce81597fe23d622f2b08cdd5a1d8686b02038
Ojelaidi <o.jelaidi@schoolyear.com>
2026-02-16 17:23:33 +0100

    bump up version and release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_schoolyear';
-$plugin->release = '1.0.18';
-$plugin->version = 2026012700;
+$plugin->release = '1.0.19';
+$plugin->version = 2026021600;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
